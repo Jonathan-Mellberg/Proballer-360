2cbdc1aaf4d6ac6598b9083b08c1a53ea7318b79
Jonathan-Mellberg <158470120+Jonathan-Mellberg@users.noreply.github.com>
2025-11-04 11:05:15 +0100

    settings started

---
+++ Assets/JoJo/JoJo Working Stuff/Scripts/settingsManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class settingsManager : MonoBehaviour
+{
+    [Header("Audio")]
+    public AudioMixer audioMixer; // Assign your mixer in the Inspector
+
+    // --- AUDIO ---
+    public void SetMasterVolume(float volume)
+    {
+        // Assuming your mixer has a parameter named "MasterVolume"
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+    }
+
+    // --- GRAPHICS QUALITY ---
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    // --- FULLSCREEN TOGGLE ---
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    // --- RETURN TO ANOTHER SCENE (like Main Menu) ---
+    public void BackToMenu(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
